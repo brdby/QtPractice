5bafbe81ea12bf339cb2f51ad737df289d95796c
Valeriy Kucherov <beardby@gmail.com>
2020-11-02 18:09:10 +0300

    csv formatting updated

--- animalcsvserializer.cpp
+++ animalcsvserializer.cpp
@@ -1,10 +1,22 @@
 #include "animalcsvserializer.h"
 
 QString AnimalCsvSerializer::toString(const Animal &animal) {
-    return QString("%1,%2\n").arg(animal.name).arg(animal.type);
+    return QString("%1,%2\n").arg(escapeCommas(animal.name)).arg(escapeCommas(animal.type));
 }
 
 Animal AnimalCsvSerializer::fromString(const QString &csvString) {
-    auto values = csvString.split(",");
-    return {values[0], values[1]};
+    QRegularExpression re("(?:^\"|,\")(\"\"|[\\w\\W]*?)(?=\",|\"$)|(?:^(?!\")|,(?!\"))([^,]*?)(?=$|,)|(\\r\\n|\\n)");
+    auto iter = re.globalMatch(csvString);
+    QStringList words;
+    while (iter.hasNext()) {
+        auto match = iter.next();
+        words << match.captured(match.lastCapturedIndex());
+    }
+    if (words.size() == 2) return {words[0], words[1]};
+    else return {};
+}
+
+QString AnimalCsvSerializer::escapeCommas(const QString &string)
+{
+    return QString("\"%1\"").arg(string);
 }
--- animalcsvserializer.h
+++ animalcsvserializer.h
@@ -2,12 +2,16 @@
 #define ANIMALCSVSERIALIZER_H
 
 #include "animalserializer.h"
+#include <QRegularExpression>
 
 class AnimalCsvSerializer : public AnimalSerializer
 {
 public:
     virtual QString toString(const Animal &animal) override;
     virtual Animal fromString(const QString &csvString) override;
+
+private:
+    QString escapeCommas(const QString &string);
 };
 
 #endif // ANIMALCSVSERIALIZER_H
