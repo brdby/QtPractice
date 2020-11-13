64bf249cdb8ebcab3211a2dec23e5d18c2209ab7
Valeriy Kucherov <beardby@gmail.com>
2020-11-13 21:20:31 +0300

    pr2: drawing algorithm fix

--- pr2/treedrawer.cpp
+++ pr2/treedrawer.cpp
@@ -10,7 +10,9 @@ void TreeDrawer::calculateInitialX(Node &node, int lvl)
     for (auto &childNode : node.childList) {
         calculateInitialX(childNode, lvl+1);
     }
-    int nodesOnCurrentLvl = nodesOnlvl.value(lvl, 0);
-    node.setX(nodesOnCurrentLvl++);
-    nodesOnlvl.insert(lvl, nodesOnCurrentLvl);
+    int nodesOnCurrentLvl = nodesOnlvl.value(lvl);
+    int nodesOnLastLvl = nodesOnlvl.value(lvl-1);
+    int acceptedX = nodesOnCurrentLvl>nodesOnLastLvl ? nodesOnCurrentLvl : nodesOnLastLvl;
+    node.setX(acceptedX++);
+    nodesOnlvl.insert(lvl, acceptedX);
 }
